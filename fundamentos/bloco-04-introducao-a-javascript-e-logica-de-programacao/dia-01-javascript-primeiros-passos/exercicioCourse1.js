3cbe646b4f30a6b1e46492de3431942a81f88158
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-08-18 18:35:28 -0300

    Adiciona exercicios de variaveis

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicioCourse1.js
@@ -0,0 +1,13 @@
+const myName = "Bruna";
+const birthCity = "Juiz de Fora";
+
+let birthYear = 1992;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+b// birthCity = "Rio de Janeiro";
+
+//Uma constante não poderá ter seu valor reatribuido.
+
