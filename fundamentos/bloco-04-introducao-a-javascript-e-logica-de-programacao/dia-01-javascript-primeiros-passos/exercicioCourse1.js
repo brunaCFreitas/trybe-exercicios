c2a5bed03d17aa98c60d8e69b3b180bd45266b1a
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-08-18 18:56:10 -0300

    Adiciona exercicios 2 e 3 do course

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicioCourse1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicioCourse1.js
@@ -1,3 +1,12 @@
+/*
+Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+*/
+
 const myName = "Bruna";
 const birthCity = "Juiz de Fora";
 
@@ -7,7 +16,6 @@ console.log(myName, birthCity, birthYear);
 
 birthYear = 2030;
 console.log(birthYear);
-b// birthCity = "Rio de Janeiro";
 
-//Uma constante não poderá ter seu valor reatribuido.
+birthCity = "Rio de Janeiro"; //Uma constante não poderá ter seu valor reatribuido.
 
