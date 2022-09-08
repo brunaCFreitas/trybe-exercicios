4b747d654d1d8deef5fb1d287d67dc0418f32e40
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-08 16:23:31 -0300

    Adiciona script do exercicio 1

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercicios-praticos/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercicios-praticos/script.js
@@ -1,1 +1,23 @@
-const botaoEnviar = document.getElementById('button-submit');
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+}
+
+function clearFilds() {
+  const formElements = document.querySelectorAll('input');
+  const textArea = document.querySelector('textarea');
+  for (let index = 0; index < formElements.length; index += 1) {
+    const userInput = formElements[index];
+    userInput = '';
+    userInput.checked = false;
+  }
+  textArea = '';
+}
+
+window.onload = function () {
+  const buttonSubmit = document.getElementById('button-submit');
+  buttonSubmit.addEventListener('click', handleSubmit);
+  const buttonClear = document.getElementById('button-clear');
+  buttonClear.addEventListener('click', clearFilds);
+}
