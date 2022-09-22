53e7650c8e84695d32780d01be05724f298451dc
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-22 18:16:53 -0300

    Adiciona exercicio 1 - Nova pessoa contratada

--- fundamentos/bloco-07-Introducao-a-javascript-es6-e-testes-unitarios/README.md
+++ fundamentos/bloco-07-Introducao-a-javascript-es6-e-testes-unitarios/README.md
@@ -1,3 +1,5 @@
 # BLOCO 7 - INTRODUÇÃO À JAVASCRIPT ES6 E TESTES UNITÁRIOS
 
-Bloco em curso.
+Neste bloco aprendemos os conceitos de `let`, `const`, `arrow functions`e `template strings`.
+Além disso, vimos `Fluxo de execução e Objetos`.
+Iniciamos o aprendizado em `Jest`e a sua aplicação em `Testes unitários`
---
+++ fundamentos/bloco-08-higher-order-functions-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicios-praticos/ex01.js
@@ -0,0 +1,20 @@
+const createPerson = (name) => {
+  const person = {
+    nameFull: name,
+    email: `${name.replace(' ', '_').toLowerCase()}@trybe.com` 
+  }
+
+  return person;
+}
+
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'),
+    id2: callback('Luiza Drumond'),
+    id3: callback('Carla Paiva'),
+  }
+  return employees;
+};
+
+console.log(newEmployees(createPerson));
