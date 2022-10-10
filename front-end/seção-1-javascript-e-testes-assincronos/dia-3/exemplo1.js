9162365e0223898b8861f21d8e502715919d3a49
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-10-10 15:10:13 -0300

    Adiciona exemplo 1 e package.json

---
+++ front-end/seção-1-javascript-e-testes-assincronos/dia-3/exemplo1.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+const consultaCep = async (cep) => {
+  try {
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    const request = await fetch(url);
+    const requestJson = await request.json();
+    console.log(requestJson);
+    const { logradouro } = requestJson;
+    console.log(logradouro);
+    return logradouro;
+  } catch(error) {
+    return error.mesage;
+  }
+};
+
+consultaCep('36026460'); // Rua Barão de Aquino
