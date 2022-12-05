d02c5370c9dcc358057be818db3307acd92c05ea
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-12-05 13:18:49 -0300

    Adiciona resolução do exercicio de exemplo: store, reducers e actions

--- frontend/7.gerenciamento-de-estado-com-redux/2.usando-o-redux-com-react/my-app/src/App.js
+++ frontend/7.gerenciamento-de-estado-com-redux/2.usando-o-redux-com-react/my-app/src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
+import { actionCreator} from '../src/redux/actions';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Contador</h1>
-      <h2>0</h2>
-      <button type="button">Incrementar</button>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const { dispatch, countState } = this.props;
+    return (
+      <div>
+        <h1>Contador</h1>
+        <h2>{ countState }</h2>
+        <button onClick={() => dispatch(actionCreator())}>Incrementa 1</button>
+        <button onClick={() => dispatch(actionCreator(5))}>Incrementa 5</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  countState: state.count,
+});
+
+export default connect(mapStateToProps)(App);
+
