c365d8296f14ff52c354447a38043182e9bcd141
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-03-07 22:46:55 -0300

    feat: adiciona resolução do exercicio 6 - B

--- backend/4.introducao-ao-desenvolvimento-web-com-node/1.node-runtime-assincrono/exercises/simpsons.js
+++ backend/4.introducao-ao-desenvolvimento-web-com-node/1.node-runtime-assincrono/exercises/simpsons.js
@@ -8,8 +8,25 @@ async function readAll() {
   strings.forEach((string) => console.log(string));
 }
 
+async function getSimpsonById(id) {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+
+  const simpsons = JSON.parse(fileContent);
+
+  const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
+
+  if(!chosenSimpson) {
+    throw new Error('id não encontrado');
+  }
+
+  return chosenSimpson;
+}
+
+
 async function main() {
   await readAll();
+  const simpson = await getSimpsonById(1);
+  console.log(simpson);
 }
 
 main();
