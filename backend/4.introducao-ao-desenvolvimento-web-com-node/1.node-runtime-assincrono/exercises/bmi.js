194b91552303d48afac82b15d15b64a839961723
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-03-07 22:35:57 -0300

    feat: adiciona resolução do exercício 5

--- backend/4.introducao-ao-desenvolvimento-web-com-node/1.node-runtime-assincrono/exercises/bmi.js
+++ backend/4.introducao-ao-desenvolvimento-web-com-node/1.node-runtime-assincrono/exercises/bmi.js
@@ -11,9 +11,58 @@ function handleBMI(weight, height) {
   return bmi;
 }
 
-const weigh = readline.questionFloat('Weight: ');
-const height = readline.questionInt('Height: ');
+const BMI_MAX_AND_MIN = {
+  'Underweight': {
+    minBMI: 0,
+    maxBMI: 18.4,
+  },
+  'Normal Weight': {
+    minBMI: 18.5,
+    maxBMI: 24.9,
+  },
+  'Overweight': {
+    minBMI: 25,
+    maxBMI: 29.9,
+  },
+  'Obese Class I': {
+    minBMI: 30.0,
+    maxBMI: 34.9,
+  },
+  'Obese Class II': {
+    minBMI: 35,
+    maxBMI: 39.9,
+  },
+  'Obese Class III': {
+    minBMI: 40,
+    maxBMI: 100,
+  },
+};
 
-const bmi = handleBMI(weigh, height);
+function handleBMIResult(bmi) {
+  const statuses = Object.keys(BMI_MAX_AND_MIN);
 
-console.log(`BMI: ${bmi.toFixed(2)}`);
+  const resultFind = statuses.find((status) => {
+    const { maxBMI, minBMI } = BMI_MAX_AND_MIN[status];
+    return bmi >= minBMI && bmi <= maxBMI;
+  });
+
+  return resultFind;
+}
+
+function main() {
+  const weigh = readline.questionFloat('Weight: ');
+  const height = readline.questionInt('Height: ');
+
+  const bmi = handleBMI(weigh, height);
+
+  const bmiResult = handleBMIResult(bmi);
+
+  console.log(`BMI: ${bmi.toFixed(2)}`);
+  console.log(`bmiResult: ${bmiResult}`);
+}
+
+main();
+
+
+
+
