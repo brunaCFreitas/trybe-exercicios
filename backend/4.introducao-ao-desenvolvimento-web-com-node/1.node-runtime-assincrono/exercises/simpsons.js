275346caaa0de9f7a77dc6df72ea467ed8d01cb8
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-03-07 22:58:53 -0300

    feat: adiciona resolução do exercício 6 - F

--- backend/4.introducao-ao-desenvolvimento-web-com-node/1.node-runtime-assincrono/exercises/simpsons.js
+++ backend/4.introducao-ao-desenvolvimento-web-com-node/1.node-runtime-assincrono/exercises/simpsons.js
@@ -56,13 +56,27 @@ async function addNelsonToFamily() {
   await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
 }
 
+async function replaceNelson() {
+  const fileContent = await fs.readFile('./simpsonsFamily.json', 'utf-8');
+
+  const simpsons = JSON.parse(fileContent);
+
+  const simpsonsWithoutNelson = simpsons.filter((simpson) => simpson.id !== '8');
+
+  const simpsonsWithMaggie = simpsonsWithoutNelson
+    .concat([{ id: '15', name: 'Maggie Simpson' }]);
+
+  return fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsWithMaggie));
+}
+
 async function main() {
   // await readAll();
   // const simpson = await getSimpsonById(1);
   // console.log(simpson);
   // filterSimpsons();
   // createSimpsonsFamily();
-  addNelsonToFamily();
+  // addNelsonToFamily();
+  replaceNelson();
 }
 
 main();
