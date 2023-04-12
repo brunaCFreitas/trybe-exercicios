37d5d2ab7f86a7b6d2f74468cf409cc5933b6359
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-04-12 15:17:52 -0300

    Adiciona resolução do 1 ao 5

--- backend/4.introducao-ao-desenvolvimento-web-com-node/4.node-js-express-e-middlewares/exercicios/src/app.js
+++ backend/4.introducao-ao-desenvolvimento-web-com-node/4.node-js-express-e-middlewares/exercicios/src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const validateName = require('./middlewares/validateName');
 
 const app = express();
+
+app.use(express.json());
+
+// endpoint para adicionar novas atividades
+app.post('/activities', validateName, (_req, res) => {
+  res.status(201).json({ message: 'Atividade criada com sucesso!' });
+});
 
 module.exports = app;
