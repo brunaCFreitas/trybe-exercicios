81e7f4fc94dd2d0368e50446b127bf1795542eb1
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-04-12 15:38:43 -0300

    Adiciona resolução do 5 ao 9

--- backend/4.introducao-ao-desenvolvimento-web-com-node/4.node-js-express-e-middlewares/exercicios/src/app.js
+++ backend/4.introducao-ao-desenvolvimento-web-com-node/4.node-js-express-e-middlewares/exercicios/src/app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const validateDificulty = require('./middlewares/validateDificulty');
+const validateRating = require('./middlewares/validateRating');
+const validateCreatedAt = require('./middlewares/validateCreatedAt');
+const validateDescription = require('./middlewares/validateDescription');
+const validatePrice = require('./middlewares/validatePrice');
 const validateName = require('./middlewares/validateName');
 
 const app = express();
@@ -6,7 +11,14 @@ const app = express();
 app.use(express.json());
 
 // endpoint para adicionar novas atividades
-app.post('/activities', validateName, (_req, res) => {
+app.post('/activities',
+  validateDificulty,
+  validateRating,
+  validateCreatedAt,
+  validateDescription,
+  validatePrice,
+  validateName,
+  (_req, res) => {
   res.status(201).json({ message: 'Atividade criada com sucesso!' });
 });
 
