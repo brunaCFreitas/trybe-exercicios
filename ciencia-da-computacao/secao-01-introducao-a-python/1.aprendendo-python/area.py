e1148c0ad28524aaa9d80bc77ef22bfc6d981d49
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-05-30 18:33:03 -0300

    fix; corrige linter

--- ciencia-da-computacao/secao-01-introducao-a-python/1.aprendendo-python/area.py
+++ ciencia-da-computacao/secao-01-introducao-a-python/1.aprendendo-python/area.py
@@ -2,21 +2,21 @@ PI = 3.14
 
 
 def square(side):
-    '''Calculate area of square'''
+    """Calculate area of square"""
     return side * side
 
 
 def rectangle(length, width):
-    '''Calculate area of rectangle'''
+    """Calculate area of rectangle"""
     return length * width
 
 
 def circle(radius):
-    '''Calculate area of circle'''
+    """Calculate area of circle"""
     return PI * (radius * radius)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Area of square:", square(10))
     print("Area of rectangle:", rectangle(2, 2))
     print("Area of circle:", circle(3))
