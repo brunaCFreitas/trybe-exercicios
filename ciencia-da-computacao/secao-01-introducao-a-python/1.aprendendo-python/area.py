9f59ba221e426591b94dc0fbc442e9667832518e
brunaCFreitas <bruna_c_freitas@hotmail.com>
2023-05-30 17:57:03 -0300

    feat: adiciona exemplos do dia

---
+++ ciencia-da-computacao/secao-01-introducao-a-python/1.aprendendo-python/area.py
@@ -0,0 +1,22 @@
+PI = 3.14
+
+
+def square(side):
+    '''Calculate area of square'''
+    return side * side
+
+
+def rectangle(length, width):
+    '''Calculate area of rectangle'''
+    return length * width
+
+
+def circle(radius):
+    '''Calculate area of circle'''
+    return PI * (radius * radius)
+
+
+if __name__ == '__main__':
+    print("Area of square:", square(10))
+    print("Area of rectangle:", rectangle(2, 2))
+    print("Area of circle:", circle(3))
